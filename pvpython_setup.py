03f0d55c7a6a893a2cff66d7c1a17ea13b16c4c2
Chris Harris <chris.harris@kitware.com>
2015-10-20 11:22:06 -0400

    Add bash script to install require packages in pvpython

---
+++ pvpython_setup.py
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Please provide path to pvpython"
+
+echo "Installing requests ..."
+
+REQUESTS_DIR=`mktemp -d`
+wget --no-check-certificate https://github.com/kennethreitz/requests/tarball/v2.8.1 -O - | tar xz -C $REQUESTS_DIR
+pushd .
+cd $REQUESTS_DIR/*requests*
+$1 setup.py install
+popd
+rm -rf $REQUESTS_DIR
+
+echo "Installing requests-toolbelt ..."
+
+# Install setuptools
+wget https://bootstrap.pypa.io/ez_setup.py -O - | $1
+
+REQUESTS_TOOLBELT_DIR=`mktemp -d`
+wget --no-check-certificate https://github.com/sigmavirus24/requests-toolbelt/tarball/0.4.0 -O - | tar xz -C $REQUESTS_TOOLBELT_DIR
+pushd .
+cd $REQUESTS_TOOLBELT_DIR/*requests-toolbelt*
+$1 setup.py install
+popd
+rm -rf $REQUESTS_TOOLBELT_DIR
+
+
